9a1e2b4c19bf11442210952271fb8e8ad5222e5c
zhangjunphy <zhangjunphy@gmail.com>
2022-11-09 14:24:00 +0800

    redo chap3 exercises with tactics

--- Exercises.lean
+++ Exercises.lean
@@ -124,10 +124,10 @@ open Classical
 variable (α : Type) (p q : α → Prop)
 variable (r : Prop)
 
-example : (∃ x : α, r) → r := 
+example : (∃ _ : α, r) → r := 
   λ h => h.elim (λ _ hr => hr)
   
-example (a : α) : r → (∃ e : α, r) :=
+example (a : α) : r → (∃ _ : α, r) :=
   λ h1 => ⟨a, h1⟩
   
 example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := 
@@ -208,7 +208,7 @@ section Chap4_2
 variable (α : Type) (p q : α → Prop)
 variable (r : Prop)
 
-example : α -> ((∃ x : α, r) ↔ r) := 
+example : α -> ((∃ _ : α, r) ↔ r) := 
   λ w => Iff.intro
     (λ ⟨_, hr⟩ => hr)
     (λ hr => ⟨w, hr⟩)
@@ -272,13 +272,10 @@ example (p q r : Prop) (hp : p)
     repeat (constructor; repeat (first | apply Or.inl; assumption | apply Or.inr | assumption))
 end Chap5_2
 
-section Chap5_1
+-- chap5 exercise 1
+section 
 variable (p q r : Prop)
 
-syntax "triv_and" : tactic
-macro_rules
-  | `(tactic| triv_and) => `(tactic| repeat (first | constructor | assumption))
-  
 syntax "triv_or_1" (colGt tactic) : tactic
 macro_rules
   | `(tactic| triv_or_1 $p) => `(tactic| (first | apply Or.inl; $p | apply Or.inr; $p))
@@ -291,7 +288,7 @@ macro_rules
 
 -- commutativity of ∧ and ∨
 example : p ∧ q ↔ q ∧ p := by
-  apply Iff.intro <;> (intro ⟨_, _⟩; triv_and)
+  apply Iff.intro <;> (intro ⟨_, _⟩; trivial)
   
 example : p ∨ q ↔ q ∨ p := by 
   apply Iff.intro <;> intro h <;> cases h <;> (rename_i hp <;> triv_or')
@@ -299,8 +296,8 @@ example : p ∨ q ↔ q ∨ p := by
 -- associativity of ∧ and ∨
 example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := by
   apply Iff.intro
-  . intro ⟨⟨_, _⟩, _⟩; triv_and
-  . intro ⟨_, _, _⟩; triv_and
+  . intro ⟨⟨_, _⟩, _⟩; trivial
+  . intro ⟨_, _, _⟩; trivial
 
 example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := by
   apply Iff.intro <;> intro h <;> cases h <;> rename_i h' <;> 
@@ -311,8 +308,8 @@ example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
   apply Iff.intro
   . intro ⟨hp, hqr⟩
     cases hqr
-    . apply Or.inl; triv_and
-    . apply Or.inr; triv_and
+    . apply Or.inl; trivial
+    . apply Or.inr; trivial
   . intro h <;> cases h <;> rename_i h' <;> match h' with
     | ⟨_, _⟩ => constructor <;> (try assumption) <;> (try triv_or')
     
@@ -325,17 +322,16 @@ example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := by
     . triv_or'
     . cases hpr
       . triv_or'
-      . apply Or.inr; triv_and
+      . apply Or.inr; trivial
 
 -- other properties
 example : (p → (q → r)) ↔ (p ∧ q → r) := by
   apply Iff.intro
   . intro h
-    intro hpq
-    match hpq with
-    | ⟨hp, hq⟩ => exact h hp hq
-  . intros h hp hq
-    apply h; triv_and
+    intro ⟨hp, hq⟩
+    exact h hp hq
+  . intro h hp hq
+    apply h; trivial
 
 example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := by 
   apply Iff.intro
@@ -347,14 +343,203 @@ example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := by
     . apply h1; assumption
     . apply h2; assumption
     
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬(p ∧ ¬p) := sorry
-example : p ∧ ¬q → ¬(p → q) := sorry
-example : ¬p → (p → q) := sorry
-example : (¬p ∨ q) → (p → q) := sorry
-example : p ∨ False ↔ p := sorry
-example : p ∧ False ↔ False := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := by
+  apply Iff.intro
+  . intro h
+    constructor <;> (intro hp; apply h; triv_or')
+  . intro ⟨_, _⟩ hpq
+    cases hpq <;> contradiction
+    
+example : ¬p ∨ ¬q → ¬(p ∧ q) := by
+  intro h ⟨_, _⟩
+  cases h <;> contradiction
+  
+example : ¬(p ∧ ¬p) := by
+  intro ⟨_, _⟩
+  contradiction
 
-end Chap5_1
+example : p ∧ ¬q → ¬(p → q) := by
+  intro ⟨hp, _⟩ h
+  let _ := h hp
+  contradiction
+  
+example : ¬p → (p → q) := by
+  intro _ _
+  contradiction
+
+example : (¬p ∨ q) → (p → q) := by
+  intro h hp
+  cases h; contradiction; assumption
+
+example : p ∨ False ↔ p := by
+  apply Iff.intro
+  . intro h
+    cases h; assumption; contradiction
+  . intro; triv_or'
+
+example : p ∧ False ↔ False := by
+  apply Iff.intro
+  . intro ⟨_, _⟩; assumption
+  . intro; constructor; contradiction; assumption
+
+example : (p → q) → (¬q → ¬p) := by
+  intro h hnq hp
+  let _ := h hp
+  contradiction
+  
+end
+
+section
+variable (p q r : Prop)
+
+example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := by
+  intro h
+  by_cases hp : p
+  . cases h hp
+    . apply Or.inl; intro; assumption
+    . apply Or.inr; intro; assumption
+  . apply Or.inl; intro; contradiction
+    
+example : ¬(p ∧ q) → ¬p ∨ ¬q := by
+  intro h
+  by_cases hp : p
+  . by_cases hq : q
+    . let _ := And.intro hp hq; contradiction
+    . triv_or'
+  . triv_or'
+  
+example : ¬(p → q) → p ∧ ¬q := by
+  intro h
+  by_cases hp : p
+  . by_cases hq : q
+    . let _ := λ _: p => hq; contradiction
+    . trivial
+  . let _: (p → q) := λ _: p => by contradiction
+    contradiction
+  
+example : (p → q) → (¬p ∨ q) := by
+  intro h
+  (by_cases p <;> try (rename_i hp; let _ := h hp)) <;> triv_or'
+
+example : (¬q → ¬p) → (p → q) := by
+  intro h hp
+  by_cases q
+  . assumption
+  . rename_i hq; let _ := h hq; contradiction
+
+example : p ∨ ¬p := by
+  by_cases p <;> triv_or'
+
+example : (((p → q) → p) → p) := by
+  intros h
+  by_cases p
+  . trivial
+  . let _ : p → q := λ p => by trivial 
+    apply h
+    trivial
+
+end
+
+section  
+variable (α : Type) (p q : α → Prop)
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
+  apply Iff.intro
+  . intro h
+    constructor <;> (intro h' ; let ⟨_, _⟩ := h h'; trivial)
+  . intro h hw
+    let _ := h.left hw
+    let _ := h.right hw
+    trivial
+    
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := by
+  intro h h' hw
+  let _ := (h hw) (h' hw)
+  trivial
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := by
+  intro h hw
+  cases h <;> (rename_i h'; let _ := h' hw; triv_or')
+end
+
+section
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : α → ((∀ x : α, r) ↔ r) := by
+  intro ha
+  apply Iff.intro
+  . intro h'
+    exact h' ha
+  . intros; trivial
+
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
+  apply Iff.intro
+  . intro h
+    by_cases hr : r
+    . triv_or'
+    . suffices ∀ (x : α), p x by triv_or'
+      intro hw
+      let _ := h hw
+      by_cases h' : (p hw)
+      . trivial 
+      . let _ : ¬(p hw ∨ r) := λ h'' => h''.elim h' hr
+        contradiction
+  . intro h
+    cases h with
+    | inl h' => intro px; exact Or.inl (h' px)
+    | inr _ => intro; triv_or'
+    
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := by
+  apply Iff.intro
+  . intro h hr hx
+    exact h hx hr
+  . intro h hx hr
+    exact h hr hx
+end
+
+section
+variable (men : Type) (barber : men)
+variable (shaves : men → men → Prop)
+
+example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := by
+  let h' := h barber
+  let _ : ¬(shaves barber barber) := by
+    intro h''
+    let _ := h'.mp h''
+    contradiction
+  let _ : ¬¬(shaves barber barber) := by
+    intro h''
+    let _ := h'.mpr h''
+    contradiction
+  contradiction
+end
+
+section
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : (∃ x : α, r) → r := by
+  intro ⟨_, _⟩
+  trivial
+
+example (a : α) : r → (∃ x : α, r) := by
+  intro hr
+  exact ⟨a, hr⟩
+  
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := by
+  apply Iff.intro
+  . intro h
+
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
+
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := sorry
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := sorry
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
+
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
+example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
+example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := sorry
+
+end
