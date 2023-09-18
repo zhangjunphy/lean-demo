1e3ef4068b7c1f0541314d39734a3de46574d1d7
jzhang <jun.zhang@amd.com>
2023-09-18 17:21:12 +0800

    finished chap8 exercise 1

--- Exercises/Chap8.lean
+++ Exercises/Chap8.lean
@@ -243,55 +243,100 @@ def inverse {f : α → β} : (b : β) → ImageOf f b → α
 namespace Hidden
 def add : Nat -> Nat -> Nat
   | m, 0 => m
-  | m, n+1 => add (m+1) n
+  | m, (Nat.succ n) => Nat.succ (add m n)
   
 def mul : Nat -> Nat -> Nat
-  | 0, _ => 0
-  | m+1, n => add n (mul m n)
+  | _, 0 => 0
+  | m, (Nat.succ n) => add (mul m n) m
   
 def exp : Nat -> Nat -> Nat
   | _, 0 => 1
   | m, n+1 => mul m (exp m n)
   
-theorem add_one : add n 1 = Nat.succ n := rfl
-theorem one_add : add 1 n = Nat.succ n := 
-  Nat.recOn (motive := λ x => add 1 x = Nat.succ x) n
-  rfl
-  (λ x h => by calc
-    add 1 (Nat.succ x) = add 1 (add 1 x) := by simp [h])
-theorem add_zero : add n 0 = n := rfl
-theorem zero_add : add 0 n = n := 
-  Nat.recOn (motive := λ x => add 0 x = x) n
-  rfl
-  (λ _ h => by simp [add, h])
-  
+theorem succ_add : ∀ (m n : Nat), add (Nat.succ m) n = Nat.succ (add m n)
+  | _, 0 => rfl
+  | m, n+1  => congrArg Nat.succ (succ_add m n)
 theorem add_succ : add m (Nat.succ n) = Nat.succ (add m n) := 
   Nat.recOn (motive := λ x => add m (Nat.succ x) = Nat.succ (add m x)) n
-  (by simp [add, Nat.succ])
-  (λ x h => by calc
-    add (x+1) (n+1) = add x (n+1+1) := by simp [add]
-    )
+  rfl
+  (λ _ _ => rfl)
   
-theorem succ_add : add (Nat.succ m) n = Nat.succ (add m n) := 
-  Nat.recOn (motive := λ x => add (Nat.succ x) n = Nat.succ (add x n)) m
-  (by trivial)
-  (λ x h => by calc
-    add ((x + 1) + 1) n = add (x + 1) (n + 1) := by simp [add]
-    _ = Nat.succ (add x (n+1)) := by apply [h])
+theorem add_zero : add m 0 = m := rfl
+theorem zero_add : ∀ m : Nat, add 0 m = m 
+  | 0 => rfl
+  | m+1 => congrArg Nat.succ (zero_add m)
   
-theorem add_comm : add m n = add n m :=
-  Nat.recOn (motive := λ x => add x n = add n x) m
-  (let h : add 0 n = add n 0 := 
-    Nat.recOn (motive := λ x => add 0 x = add x 0) n
-    (by trivial)
-    (λ x h => by calc
-      add 0 (Nat.succ x) = Nat.succ x := by simp [add]
-      _ = Nat.succ (add x 0) := by simp [add, ←h]
-      _ = add (Nat.succ x) 0 := sorry)
-   h) 
-  sorry
-  
-example : exp a (add b c) = mul (exp a b) (exp a c) := 
-  sorry
+theorem add_comm : ∀ (m n : Nat), add m n = add n m 
+  | _, 0 => by simp [zero_add, add_zero]
+  | m, n+1 => by (rw [succ_add, add_succ]; 
+                  exact congrArg Nat.succ (add_comm m n))
+                  
+theorem add_one : ∀ n : Nat, add n 1 = Nat.succ n
+  | 0 => rfl
+  | n+1 => congrArg Nat.succ (add_one n)
 
+theorem one_add : ∀ n : Nat, add 1 n = Nat.succ n 
+  | 0 => rfl
+  | n+1 => congrArg Nat.succ (one_add n)
+                  
+theorem add_assoc : ∀ (a b c : Nat), add (add a b) c = add a (add b c)
+  | _, 0, _ => by simp [add_zero, zero_add]
+  | a, b+1, c => by simp [succ_add, add_succ, add_assoc a b c]
+
+theorem mul_one : ∀ n : Nat, mul n 1 = n
+  | 0 => rfl
+  | n+1 => congrArg Nat.succ (mul_one n) 
+  
+theorem one_mul : ∀ n : Nat, mul 1 n = n
+  | 0 => rfl
+  | n+1 => congrArg Nat.succ (one_mul n)
+  
+theorem mul_zero : mul n 0 = 0 := rfl
+theorem zero_mul : ∀ n : Nat, mul 0 n = 0
+  | 0 => rfl
+  | n+1 => by simp [mul, add_zero]; exact zero_mul n
+
+theorem mul_succ : mul m (Nat.succ n) = add (mul m n) m := rfl
+
+theorem mul_dist : ∀ (b c : Nat), mul a (add b c) = add (mul a b) (mul a c)
+  | _, 0 => by simp [add]
+  | b, c+1 => by simp [add_succ, mul_succ, mul_dist b c, add_assoc]
+
+theorem succ_mul : mul (Nat.succ m) n = add (mul m n) n :=
+  Nat.recOn (motive := λ x => mul (Nat.succ m) x = add (mul m x) x) n
+  rfl
+  (λ x h => by calc
+    mul (Nat.succ m) (Nat.succ x) = add (mul (Nat.succ m) x) (Nat.succ m) := by simp [mul] 
+    _ = add (add (mul m x) x) (Nat.succ m) := by rw [h]
+    _ = add (add (mul m x) x) (add m 1) := rfl
+    _ = add (mul m x) (add x (add m 1)) := by rw [add_assoc]
+    _ = add (mul m x) (add (add x m) 1) := by rw [add_assoc, add_comm]
+    _ = add (mul m x) (add m (add x 1)) := by simp [add_comm, add_assoc]
+    _ = add (add (mul m x) m) (add x 1) := by simp [add_assoc]
+    _ = add (mul m (add x 1)) (add x 1) := by simp [mul_succ, add])
+
+theorem mul_comm : ∀ (m n : Nat), mul m n = mul n m 
+  | _, 0 => by simp [mul_zero, zero_mul]
+  | m, n+1 => by simp [mul_succ, succ_mul, mul_comm m n]
+
+theorem mul_assoc : ∀ (a b c : Nat), mul (mul a b) c = mul a (mul b c)
+  | _, 0, _ => by simp [mul_zero, zero_mul]
+  | a, b+1, c => 
+    let h : mul c (mul a b) = mul a (mul b c) := by { calc
+      mul c (mul a b) = mul (mul a b ) c := by simp [mul_comm]
+      _ = mul a (mul b c) := by simp [mul_assoc a b c]
+    }
+    by simp [mul_succ, succ_mul, mul_comm, mul_dist, h]
+    
+theorem mul_exp : ∀ a : Nat, mul n (exp n a) = exp n (Nat.succ a)
+  | 0 => rfl
+  | a+1 => congrArg (mul n) (mul_exp a)
+
+theorem exp_mul : ∀ a : Nat, mul (exp n a) n = exp n (Nat.succ a)
+  | 0 => by simp [exp, mul_one, one_mul]
+  | a+1 => by simp [exp, mul_comm]
+  
+theorem exp_add : ∀ (a b c : Nat), exp a (add b c) = mul (exp a b) (exp a c)
+  | _, 0, _ => by simp [add_zero, zero_add, exp, mul_one, one_mul]
+  | a, b+1, c => by simp [succ_add, exp, exp_add a b c, mul_assoc]
 end Hidden
